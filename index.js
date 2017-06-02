a0f3af383d2d5f41a223f980ed64f57eadb7afb9
Laurin Quast <laurinquast@googlemail.com>
2017-06-02 17:51:28 +0200

    docs(*): Fixes production build

--- index.js
+++ index.js
@@ -2,18 +2,25 @@
 'use strict'
 
 const path = require(`path`)
-
+const Funnel = require(`broccoli-funnel`)
+const MergeTrees = require(`broccoli-merge-trees`)
 const addonDefinition = {
   name: 'ember-card-stacks',
   afterInstall() {
     return this.addPackageToProject(`animejs`)
   },
-  included(app) {
+  included() {
     this._super.included.apply(this, arguments)
     this.import(`app/styles/ember-card-stacks.css`)
-    this.import(path.join(path.dirname(require.resolve(`animejs`)), `anime.js`))
+    this.import(`vendor/anime.min.js`)
     this.import(`vendor/shims/animejs.js`)
   },
+  treeForVendor(vendorTree) {
+    const animeTree = new Funnel(path.join(this.project.root, `node_modules`, `animejs`), {
+      files: [ `anime.min.js` ],
+    })
+    return new MergeTrees([ vendorTree, animeTree ])
+  }
 }
 
 module.exports = addonDefinition
