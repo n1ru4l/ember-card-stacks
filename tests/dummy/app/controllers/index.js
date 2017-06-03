f9d0eca43e14ce3e569620b8aff832d4a7b492ee
Laurin Quast <laurinquast@googlemail.com>
2017-06-03 15:52:43 +0200

    docs(animationFunctions): Adds alternative animation

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,6 +1,7 @@
 import Controller from 'ember-controller'
 import computed from 'ember-computed'
 import EObject from 'ember-object'
+import run from 'ember-runloop'
 import { A as EArray } from 'ember-array/utils'
 
 function getRandomColor() {
@@ -17,10 +18,56 @@ for (let i = 0; i < 20; i++) {
   )
 }
 
+const createCardFadeAnimation1 = () => ({
+  translateX: {
+    value: 1024,
+    duration: 350,
+  },
+  rotate: {
+    value: `10deg`,
+    duration: 350,
+  },
+  opacity: {
+    value: .3,
+    duration: 200,
+  },
+  easing: `easeInQuad`,
+})
+const createCardFadeAnimation2 = () => ({
+  translateY: {
+    value:1024,
+    duration: 350,
+  },
+  translateX: {
+    value: -2050,
+    duartion: 350,
+  },
+  rotate: {
+    value: `10deg`,
+    duration: 350,
+  },
+  scale: {
+    value: .9,
+  },
+  opacity: {
+    value: .3,
+    duration: 350,
+  },
+  easing: `easeInQuad`,
+})
+
+const fadeAnimationFunctions = EArray([
+  createCardFadeAnimation1,
+  createCardFadeAnimation2,
+])
+
 export default Controller.extend({
   items,
   currentItemIndex: 0,
   visibleItemAmount: 3,
+  fadeAnimationFunctions,
+  currentFadeAnimationFunctionIndex: 0,
+  // computed state
   maxItemIndex: computed(`items.length`, function () {
     return this.get(`items.length`) - 1
   }),
@@ -30,10 +77,25 @@ export default Controller.extend({
   isDecrementDisabled: computed(`currentItemIndex`, function () {
     return this.get(`currentItemIndex`) === 0
   }),
+  createCardFadeAnimation: computed(`fadeAnimationFunctions`, `currentFadeAnimationFunctionIndex`, function () {
+    const fadeAnimationFunctions = this.get(`fadeAnimationFunctions`)
+    const currentFadeAnimationFunctionIndex = this.get(`currentFadeAnimationFunctionIndex`)
+    return fadeAnimationFunctions.objectAt(currentFadeAnimationFunctionIndex)
+  }),
   actions: {
     next() {
-      const item = items.shiftObject()
-      items.pushObject(item)
+      this.set(`currentFadeAnimationFunctionIndex`, 0)
+      run.next(() => {
+        const item = items.shiftObject()
+        items.pushObject(item)
+      })
+    },
+    nextAnimated() {
+      this.set(`currentFadeAnimationFunctionIndex`, 1)
+      run.next(() => {
+        const item = items.shiftObject()
+        items.pushObject(item)
+      })
     },
     increment() {
       const isIncrementDisabled = this.get(`isIncrementDisabled`)
